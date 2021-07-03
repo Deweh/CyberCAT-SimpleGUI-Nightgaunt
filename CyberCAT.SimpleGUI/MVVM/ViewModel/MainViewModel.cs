afe66c3480688a941f3036c5e3a9374f9626ec74
Deweh <76853940+Deweh@users.noreply.github.com>
2021-07-03 06:06:21 -0400

    Add WPF boilerplate code & finish MainWindow layout.

---
+++ CyberCAT.SimpleGUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CyberCAT.SimpleGUI.Core;
+
+namespace CyberCAT.SimpleGUI.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand PlayerStatsViewCommand { get; set; }
+
+        public PlayerStatsViewModel PlayerStatsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            PlayerStatsVM = new PlayerStatsViewModel();
+
+            PlayerStatsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PlayerStatsVM;
+            });
+        }
+    }
+}
--- CyberCAT.SimpleGUI/MainWindow.xaml.cs
+++ CyberCAT.SimpleGUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace CyberCAT.SimpleGUI
 {
@@ -23,6 +24,21 @@ namespace CyberCAT.SimpleGUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void loadSave_Click(object sender, RoutedEventArgs e)
+        {
+            var openDialog = new OpenFileDialog
+            {
+                InitialDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Saved Games", "CD Projekt Red", "Cyberpunk 2077"),
+                Filter = "Cyberpunk 2077 Save File|*.dat"
+            };
+
+            if (openDialog.ShowDialog() == true)
+            {
+                MessageBox.Show("Opened!");
+            }
+
         }
     }
 }
